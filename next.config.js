6a6e649ec93414276ca0594a634990a1b85901a7
alaa.alhaidar <alaa.alhaidar@hotmail.ccom>
2026-02-25 10:41:04 +0100

    admin api

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/admin',
+        destination: '/api/admin-panel',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
